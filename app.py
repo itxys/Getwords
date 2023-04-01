5845de1167800755ea89b3064dd4d1725783e866
itxys <itxys@qq.com>
2023-04-02 01:15:19 +0800

    initial commit

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask, render_template, request, jsonify
+from get_words import GetWords
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/api/hello')
-def hello():
-    return jsonify({'message': 'Hello, World!'})
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/getwords', methods=['POST'])
+def get_words():
+    text = request.form['text']
+    result = GetWords(text)
+    return jsonify(result)
 
 if __name__ == '__main__':
+    app.run(debug=True)
     app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ get_words.py
@@ -0,0 +1,6 @@
+class GetWords:
+    def __init__(self):
+        pass
+
+    def get_words(self, text):
+        return text.split()
